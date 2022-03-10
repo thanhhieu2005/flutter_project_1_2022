e0eb185229263badf5489b24d23de9f296398e41
KSB-tqk <linh05602@gmail.com>
2022-03-10 17:02:07 +0700

    split main.dart into app.dart

---
+++ lib/app.dart
@@ -0,0 +1,48 @@
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_project_1/services/auth_service.dart';
+import 'package:flutter_project_1/view_models/locale_provider.dart';
+import 'package:flutter_project_1/views/landing/landing_page.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:provider/provider.dart';
+
+import 'l10n/support_locale.dart';
+
+class VAtractionApp extends StatelessWidget {
+  const VAtractionApp({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+        designSize: const Size(414, 736),
+        builder: () {
+          return MultiProvider(
+              providers: [
+                Provider<AuthService>(
+                  create: (_) => AuthService(),
+                ),
+              ],
+              child: MaterialApp(
+                title: 'Flutter Demo',
+                theme: ThemeData(
+                  primarySwatch: Colors.blue,
+                ),
+                // ignore: prefer_const_literals_to_create_immutables
+                localizationsDelegates: [
+                  AppLocalizations.delegate,
+                  GlobalMaterialLocalizations.delegate,
+                  GlobalWidgetsLocalizations.delegate,
+                  GlobalCupertinoLocalizations.delegate,
+                ],
+                locale: context.watch<LocaleProvider>().locale,
+                // ignore: prefer_const_literals_to_create_immutables
+                supportedLocales: L10n.support,
+                debugShowCheckedModeBanner: false,
+                home: const LandingPage(),
+              ));
+        });
+  }
+}
