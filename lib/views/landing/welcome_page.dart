73115f648788099c651b929b0cbd23677687b42a
Khanh Tran <khanhsbtqk@gmail.com>
2022-06-01 11:00:42 +0700

    commit to pull

--- lib/views/landing/welcome_page.dart
+++ lib/views/landing/welcome_page.dart
@@ -25,92 +25,96 @@ class WelcomePage extends StatefulWidget {
 class _WelcomePageState extends State<WelcomePage> {
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
     return Scaffold(
       body: SafeArea(
         child: Stack(
+          fit: StackFit.loose,
           children: [
-            Column(
-              children: [
-                Text(
-                  AppLocalizations.of(context).findInterest,
-                  style: TextConfigs.kText22SemiBoldPrimary,
+            Container(
+              padding: EdgeInsets.only(top: 80.h),
+              child: Center(
+                child: Image.asset(
+                  "assets/images/bg_welcome.png",
+                  fit: BoxFit.cover,
                 ),
-                SizedBox(
-                  width: 30.h,
-                ),
-                Text(
-                  AppLocalizations.of(context).placeToDiscover,
-                  style: TextConfigs.kText22SemiBoldPrimary,
-                ),
-                Text(
-                  AppLocalizations.of(context).weShareAll,
-                  style: TextConfigs.kText16Secondary,
-                ),
-                Container(
-                  color: AppColors.kBackgroundColor,
-                  child: Center(
-                    child: Column(
-                      children: [
-                        Image.asset(
-                          "assets/images/bg_welcome.png",
-                          fit: BoxFit.fill,
-                          height: 600.h,
-                          width: size.width,
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
+              ),
             ),
-            Positioned(
-              bottom: 0,
-              left: 0,
-              right: 0,
-              child: Column(
-                children: [
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 40.w),
-                    child: RoundedLinearButton(
-                        text: AppLocalizations.of(context).signUp,
-                        press: () {
-                          Navigator.push(context, SignUpPage.route());
-                        },
-                        isAllCap: false,
-                        textColor: Colors.white,
-                        startColor: AppColors.kPrimaryColor,
-                        endColor: AppColors.kPrimaryColor),
-                  ),
-                  SizedBox(
-                    height: 10.h,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        AppLocalizations.of(context).alreadyHaveAccount,
-                        style: TextConfigs.kText16Secondary,
+            Container(
+              padding: EdgeInsets.only(left: 20.w, top: 20.h),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      AppLocalizations.of(context).findInterest,
+                      style: TextConfigs.kText22SemiBoldPrimary,
+                    ),
+                    SizedBox(
+                      width: 30.h,
+                    ),
+                    Text(
+                      AppLocalizations.of(context).placeToDiscover,
+                      style: TextConfigs.kText22SemiBoldPrimary,
+                    ),
+                    Text(
+                      AppLocalizations.of(context).weShareAll,
+                      style: TextConfigs.kText16Secondary,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Positioned.fill(
+              child: Align(
+                alignment: Alignment.bottomCenter,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 40.w,
                       ),
-                      SizedBox(
-                        width: 10.w,
+                      child: RoundedLinearButton(
+                          text: AppLocalizations.of(context).signUp,
+                          press: () {
+                            Navigator.push(context, SignUpPage.route());
+                          },
+                          isAllCap: false,
+                          textColor: Colors.white,
+                          startColor: AppColors.kPrimaryColor,
+                          endColor: AppColors.kPrimaryColor),
+                    ),
+                    Container(
+                      padding: EdgeInsets.only(top: 10.h, bottom: 15.h),
+                      color: AppColors.kBackgroundColor,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            AppLocalizations.of(context).alreadyHaveAccount,
+                            style: TextConfigs.kText16Secondary,
+                          ),
+                          SizedBox(
+                            width: 10.w,
+                          ),
+                          GestureDetector(
+                            onTap: () {
+                              Navigator.pushNamedAndRemoveUntil(context,
+                                  LoginPage.nameRoute, (route) => false);
+                            },
+                            child: Text(
+                              AppLocalizations.of(context).signIn,
+                              style: TextConfigs.kText16Primary,
+                            ),
+                          ),
+                        ],
                       ),
-                      GestureDetector(
-                        onTap: () {
-                          Navigator.pushNamedAndRemoveUntil(
-                              context, LoginPage.nameRoute, (route) => false);
-                        },
-                        child: Text(
-                          AppLocalizations.of(context).signIn,
-                          style: TextConfigs.kText16Primary,
-                        ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(
-                    height: 10.h,
-                  ),
-                ],
+                    ),
+                  ],
+                ),
               ),
             ),
           ],
